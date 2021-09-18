6cf4c93700b0a06d22625a7b65f371eacb8f395f
DTKingdom <dylantobar2001@gmail.com.com>
2021-09-18 17:11:03 -0600

    cambio de bd

--- configurations/db-config.js
+++ configurations/db-config.js
@@ -1,7 +1,7 @@
-const host= "y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-const user= "dyc49hxe9vxlzzi6";
-const password= "p1u6pjb0m7lc10fe";
-const database= "o40psjtoqny37wqz";
+const host= "j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+const user= "p3u55u0nuazag777";
+const password= "i3tivaaoxudqf4jn";
+const database= "wwpcyt9y8klyvijr";
 
 exports.host=host;
 exports.user=user;
